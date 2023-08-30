d4863fbb7408874293ff66b781e6f30b2e7cd390
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-08-31 01:43:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,123 @@
+// show menu
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// change background header
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+// show scroll up
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+// scroll section active link
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionClass = document.querySelector(
+      `.nav__menu a[href*=${sectionId}]`
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
+
+// dark light theme
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
+
+// const selectedTheme = localStorage.getItem("selected-theme");
+// const selectedIcon = localStorage.getItem("selected-icon");
+
+// const getCurrentTheme = () => {
+//   document.body.classList.contains(darkTheme) ? "dark" : "light";
+// };
+
+// const getCurrentIcon = () => {
+//   document.body.classList.contains(iconTheme) ? "ri-sun-line" : "ri-moon-line";
+// };
+
+// if (selectedTheme) {
+//   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+//     darkTheme
+//   );
+
+//   themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+//     iconTheme
+//   );
+// }
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  // localStorage.setItem("selected-theme", getCurrentTheme());
+  // localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+// scroll reveal animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true,
+});
+
+sr.reveal(
+  `.home__img,.newsletter__container,.footer__logo,.footer__description,.footer__content,.footer__info`
+);
+sr.reveal(`.home__data`, { origin: "bottom" });
+sr.reveal(`.about__data,.recently__data`, { origin: "left" });
+sr.reveal(`.about__img,.recently img`, { origin: "right" });
+sr.reveal(`.popular__card`, { interval: 100 });
